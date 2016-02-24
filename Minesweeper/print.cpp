be7737dae7895984349f802ad537b6d8eb0b6309
Dirk Reinbach <dirk.reinbach@gmail.com>
2016-02-24 10:07:59 +0100

    Simplify printing code.

--- Minesweeper/print.cpp
+++ Minesweeper/print.cpp
@@ -8,35 +8,34 @@ namespace minesweeper
 {
 namespace
 {
-char const kStateChar[2][19] = { "##################", "XXXXXXXXX.12345678" };
-
+char const kStateChar[2][19] =
+{
+  "##################", "XXXXXXXXX.12345678"
+};
 string ToString(State const& state)
 {
-  return{ kStateChar[get<Uncovered>(state)][get<MineCount>(state) + 9] };
+  return { kStateChar[get<Uncovered>(state)][get<MineCount>(state) + 9] };
 }
-void PrintLine(pair<Board::const_iterator, Board::const_iterator> range)
+auto Row(Board::const_iterator iterator)
 {
-  for_each(get<0>(range), get<1>(range), [](auto const& cell)
-  {
-    Print(ToString(get<State>(cell)));
-  });
+  return get<0>(get<Position const>(*iterator));
+}
+auto PrintRow(Board::const_iterator begin, Board::const_iterator end)
+{
+  auto last = begin;
+  while (begin != end && Row(last) == Row(begin))
+    Print(ToString(get<State>(*(last = begin++))));
+
   Print("\n");
-}
-auto FirstRow(Board::const_iterator begin, Board::const_iterator end)
-{
-  auto const first_row = get<0>(get<Position const>(*begin));
-  return make_pair(begin, find_if_not(begin, end, [=](auto const& cell)
-  {
-    return get<0>(get<Position const>(cell)) == first_row;
-  }));
+  return begin;
 }
 }
 
 void Print(Board const& board)
 {
-  auto row = make_pair(begin(board), begin(board));
-  while (get<1>(row) != end(board))
-    PrintLine(row = FirstRow(get<1>(row), end(board)));
+  auto row_end = begin(board);
+  while (row_end != end(board))
+    row_end = PrintRow(row_end, end(board));
 }
 void Print(std::string const& str)
 {
