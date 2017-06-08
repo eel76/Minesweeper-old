6b5da550a38110c3c599a980a76884f7e253f5b4
Dirk Reinbach <reinbach@volumegraphics.com>
2017-06-08 17:31:34 +0200

    Refactoring in progress

--- evaluation.cpp
+++ evaluation.cpp
@@ -6,13 +6,13 @@ using namespace std;
 bool minesweeper::gameLost(Board board)
 {
   return any_of(begin(board), end(board), [](auto cell) {
-    return get<Uncovered>(get<State>(cell)) && isMine(get<State>(cell));
+    return get<Uncovered>(get<State>(cell)) & isMine(get<State>(cell));
   });
 }
 
 bool minesweeper::gameWon(Board board)
 {
   return all_of(begin(board), end(board), [](auto cell) {
-    return get<Uncovered>(get<State>(cell)) || isMine(get<State>(cell));
+    return get<Uncovered>(get<State>(cell)) ^ isMine(get<State>(cell));
   });
 }
