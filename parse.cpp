fcf1dc66d7b5f1d385c82eed4403de385a66cabe
Dirk Reinbach <reinbach@volumegraphics.com>
2017-06-23 16:00:30 +0200

    Refactoring in progress

--- parse.cpp
+++ parse.cpp
@@ -5,25 +5,30 @@
 using namespace minesweeper;
 using namespace std;
 
+template <class Value>
+using Generator = std::function<Value()>;
+
 Position minesweeper::parsePosition(std::string text)
 {
   auto match = smatch{};
-  regex_match(text, match, regex{ " *([1-9]*[0-9]) *[,; ] *([1-9]*[0-9]) *" });
+  regex_match(text, match,
+              regex{ "^ *([1-9]*[0-9]) *[,; ] *([1-9]*[0-9]) *$" });
 
   auto positions =
-  map<size_t, Position>{ { 0, Position{ Row::Invalid, Column::Invalid } },
-                         { 3, Position{ Row(stoi("0"s + match[1])),
-                                        Column(stoi(match[2])) } } };
+  map<bool, Generator<Position>>{ { false, [] { return Position{}; } } };
+
+  { { 0, Position{ Row::Invalid, Column::Invalid } },
+    { 3, Position{ Row(stoi(match[1])), Column(stoi(match[2])) } } };
   return positions[match.size()];
 }
 
 Move minesweeper::parseMove(std::string text)
 {
   auto match = smatch{};
-  regex_match(text, match, regex{ " *(m?|mark) *(.+)" });
+  regex_match(text, match, regex{ "^ *(m?|mark)(.+)$" });
 
-  auto actions = map<string, Action>{ { "", Action::Uncover },
-                                      { "m", Action::ToggleMark },
-                                      { "mark", Action::ToggleMark } };
-  return Move{ actions[match[1]], parsePosition(match[2]) };
+  auto action = map<string, Action>{ { "", Action::Uncover },
+                                     { "m", Action::ToggleMark },
+                                     { "mark", Action::ToggleMark } };
+  return Move{ action[match[1]], parsePosition(match[2]) };
 }
