9a930de3d3db955bd1b1faf1b87bc7f68f0a3d73
Dirk Reinbach <dirk.reinbach@gmail.com>
2017-06-28 22:48:15 +0200

    Refactoring in progress

--- filter.h
+++ filter.h
@@ -8,6 +8,21 @@ namespace minesweeper
   using Predicate = std::function<bool(Position)>;
   Predicate operator!(Predicate predicate);
   Positions operator|(Positions positions, Predicate predicate);
+
+  using Filter = std::function<bool(Board, Position)>;
+  Filter operator!(Filter filter);
+
+  using Range = std::pair<Board, Positions>;
+  Range operator|(Range range, Filter filter);
+
+  Range all(Board board);
+
+  Filter inBounds();
+  Filter deadly();
+  Filter revealed();
+  Filter covered();
+  Filter marked();
+  Filter noThreat();
 }
 
 namespace minesweeper
