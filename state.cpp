6ce090edcd561a117f2630405de8ca65347863ee
Dirk Reinbach <reinbach@volumegraphics.com>
2017-06-28 17:49:20 +0200

    Refactoring in progress

--- state.cpp
+++ state.cpp
@@ -4,12 +4,30 @@
 using namespace minesweeper;
 using namespace std;
 
+template <typename... Args>
+auto option(Args... args)
+{
+  return make_pair(args...);
+}
+
+template <typename... Args>
+auto mapping(Args... args)
+{
+  return std::map<State, State>{ args... };
+}
+
 State minesweeper::reveal(State state)
 {
-  auto revealed = map<State, State>{ make_pair(State::Covered, State::Revealed),
-                                     make_pair(State::Marked, State::Marked),
-                                     make_pair(State::Revealed, State::Revealed) };
-  return revealed[state];
+  return mapping(option(State::Covered, State::Revealed),
+                 option(State::Marked, State::Marked),
+                 option(State::Revealed, State::Revealed))[state];
+
+  // auto revealed = map<State, State>{ make_pair(State::Covered,
+  // State::Revealed),
+  //                                    make_pair(State::Marked, State::Marked),
+  //                                    make_pair(State::Revealed,
+  //                                    State::Revealed) };
+  // return revealed[state];
 }
 
 State minesweeper::toggleMark(State state)
