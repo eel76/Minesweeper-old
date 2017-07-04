e543be67af838ff3af72bd3ce9abc6a3c222f775
Dirk Reinbach <reinbach@volumegraphics.com>
2017-07-04 13:43:36 +0200

    Refactoring in progress

--- column.cpp
+++ column.cpp
@@ -1,7 +1,5 @@
 #include "column.h"
 
-using namespace minesweeper;
-
 int minesweeper::index(Column column)
 {
   return int(column);
@@ -10,9 +8,4 @@ int minesweeper::index(Column column)
 std::string minesweeper::to_string(Column column)
 {
   return std::to_string(index(column));
-}
-
-Column minesweeper::shift(Column column, ColumnOffset offset)
-{
-  return Column(index(column) + int(offset));
 }
--- column.h
+++ column.h
@@ -7,9 +7,3 @@ namespace minesweeper
   int         index(Column column);
   std::string to_string(Column column);
 }
-
-namespace minesweeper
-{
-  enum struct ColumnOffset { Previous = -1, Current = 0, Next = +1 };
-  Column shift(Column column, ColumnOffset offset);
-}
