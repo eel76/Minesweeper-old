655e5825f5af69940ef00316fab176bbf7eefc3c
Dirk Reinbach <dirk.reinbach@gmail.com>
2017-11-23 21:58:33 +0100

    Refactoring in progress

--- severity.h
+++ severity.h
@@ -5,14 +5,14 @@
 namespace minesweeper {
   using Mines = std::map<Hazard, int>;
 
-  inline Mines mined(Mines severity, Hazard hazard) {
-    severity[hazard]++;
-    return severity;
+  inline Mines mined(Mines mines, Hazard hazard) {
+    mines[hazard]++;
+    return mines;
   }
-  inline bool isDeadly(Mines severity) {
-    return severity[Hazard::Deadly] != 0;
+  inline bool isDeadly(Mines mines) {
+    return mines[Hazard::Deadly] != 0;
   }
-  inline bool isNegligible(Mines severity) {
-    return severity.empty();
+  inline bool isNegligible(Mines mines) {
+    return mines.empty();
   }
 }
