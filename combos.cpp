27d178042d47fc26c74a0502d9957af5970363f3
Dirk Reinbach <dirk.reinbach@gmail.com>
2020-03-13 16:06:27 +0100

    Simplify combos().

--- combos.cpp
+++ combos.cpp
@@ -1,32 +1,13 @@
 #include "combos.h"
-#include <initializer_list>
-
-namespace {
-  template <class Container>
-  Container join(std::initializer_list<Container> containerLists) {
-    auto joined = Container{};
-
-    for (auto container : containerLists)
-      joined.insert(end(joined), begin(container), end(container));
-
-    return joined;
-  }
-}
-
-auto minesweeper::combos(Rows rows, Column column) -> Positions {
-  auto positions = Positions{};
-
-  for (auto row : rows)
-    positions = join({ positions, Positions{ Position{ row, column } } });
-
-  return positions;
-}
 
 auto minesweeper::combos(Rows rows, Columns columns) -> Positions {
-  auto positions = Positions{};
+  if (rows.empty())
+    return {};
+
+  auto positions = combos({ begin(rows) + 1, end(rows) }, columns);
 
   for (auto column : columns)
-    positions = join({ positions, combos(rows, column) });
+    positions.emplace_back(rows[0], column);
 
   return positions;
 }
--- combos.h
+++ combos.h
@@ -3,8 +3,6 @@
 #include "positions.h"
 #include "rows.h"
 
-namespace minesweeper
-{
-  Positions combos(Rows rows, Column column);
+namespace minesweeper {
   Positions combos(Rows rows, Columns columns);
 }
